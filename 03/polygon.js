2405e6f8b2dd62bdd92dfd40a4ce63ec519675c2
MahshadOliaie <mahshadoliaie@gmail.com>
2023-07-18 00:11:07 +0330

    calculate space and perimeter

---
+++ 03/polygon.js
@@ -0,0 +1,14 @@
+
+
+let width = prompt("enter the width");
+let height = prompt("enter the height");
+
+let w = width*1;
+let h = height*1;
+
+if(width==height){
+    alert("space of square: "+ (width*height)  );
+}
+else{
+    alert("perimeter of rectangle: " + (2* (h+w)));
+}
