46163e6ec6d51427ba239a0811259d333eef602c
MahshadOliaie <mahshadoliaie@gmail.com>
2023-07-22 23:27:07 +0330

    reverse string with style

---
+++ 07/function.js
@@ -0,0 +1,10 @@
+
+
+
+let word = "";
+function reverse(array){
+    let n = array.length;
+    for(let i=n-1; i>=0 ; i--){
+        word+=array[i];
+    }
+}
---
+++ 07/main.js
@@ -0,0 +1,23 @@
+
+
+let array = prompt("enter a word");
+
+
+reverse(array);
+
+document.body.innerText=word;
+
+
+
+
+// style
+
+document.body.style.background="darkblue";
+document.body.style.color="white";
+document.body.style.fontSize="50px";
+document.body.style.fontFamily="arial";
+document.body.style.display="flex";
+document.body.style.justifyContent="center";
+document.body.style.alignItems="center";
+document.body.style.height="100vh";
+
