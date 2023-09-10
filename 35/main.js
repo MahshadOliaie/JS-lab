a3ecf6ed3b2fc4be3c0da190277d15e7bb459388
MahshadOliaie <mahshadoliaie@gmail.com>
2023-09-11 01:43:12 +0330

    show details of products added

--- 35/main.js
+++ 35/main.js
@@ -4,10 +4,18 @@ let root = document.querySelector(".root");
 
 
 
-function products() {
-    fetch('https://fakestoreapi.com/products')
+function products(id) {
+    if(id){
+        fetch(`https://fakestoreapi.com/products/${id}`)
+        .then(Response => Response.json())
+        .then(data => showProduct(data));
+    }
+    else{
+        fetch(`https://fakestoreapi.com/products`)
         .then(Response => Response.json())
         .then(data => render(data));
+    }
+    
 }
 
 
@@ -15,9 +23,9 @@ function products() {
 
 function render(data) {
     let template = data.map(product => {
-        const { title, image, price, category, rating } = product;
+        const { title, image, price, category, rating ,id } = product;
 
-        return `<div class="card">
+        return `<div class="card" onclick="products(${id})">
         <div class="image">
             <img src="${image}" alt="">
         </div>
@@ -32,6 +40,28 @@ function render(data) {
     }).join("");
 
     root.innerHTML = template;
+}
+
+
+
+function showProduct(data) {
+    const { title, image, price, category, rating , description } = data;
+
+    root.innerHTML = `<div class="mainImg">
+    <img src="${image}" alt="">
+</div>
+<div class="aboutProduct">
+    <h2 class="titleProduct">${title}</h2>
+    <p class="description">${description}</p>
+    <p class="categoryProduct">${category}</p>
+    <p class="ratingProduct">${rating.rate}</p>
+    <p class="priceProduct">${price}</p>
+    <button class="shop">ADD</button>
+</div>`;
+
+    root.classList.add("showProduct");
+    root.classList.remove("allProducts");
+
 }
 
 
